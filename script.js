9634cc81765a12846e5e7833d17a6236a0e6a563
jmolloy501 <70666253+jmolloy501@users.noreply.github.com>
2021-03-10 16:32:40 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,71 @@
-require(["esri/Map", "esri/views/MapView"], function(Map, MapView) {
-        var map = new Map({
-          basemap: "dark-gray"
-        });
+require([
+        "esri/config",
+        "esri/Map",
+        "esri/layers/FeatureLayer",
+        "esri/views/MapView",
+        "esri/WebMap",
+        "esri/widgets/Legend",
+        "dojo/domReady!"], function(esriConfig, Map, FeatureLayer, MapView, WebMap, Legend) {
+  esriConfig.apiKey = "AAPK67d1cbf28ed14c1c804f49a6042c44fd2XwMIGFEEdYuovdTBQtKKb1PMP69IoYvFscedg6eV5dZFRTrIGP1cEltoyLjQkd-";
 
-        var view = new MapView({
-          container: "viewDiv",
-          map: map,
-          zoom: 10,
-          center: [-89.85, 38.85] // longitude, latitude
+        var map = new Map({
+          basemap: "arcgis-topographic" // Basemap layer service
         });
+        
+        var view = new MapView({
+          map: map,
+          center: [-92, 38.5], // Longitude, latitude
+          zoom: 8, // Zoom level
+          container: "viewDiv" // Div element
+        });
+        
+  
+  var template = {
+        title: "Individuals in parcel: {number_peo}",
+        content: [{
+         
+          type: "fields",
+          fieldInfos: [{
+            fieldName: "number_peo",
+            label: "Unique pings: ",
+            visible: true
+          }, {
+            fieldName: "USE_CODE_S",
+            label: "Parcel use code: ",
+            visible: true,
+            format: {
+              digitSeparator: true,
+              places: 0
+            }
+          }]
+        }]
+      };
+  var parcels_4pm = new FeatureLayer({
+        url: "https://services2.arcgis.com/bB9Y1bGKerz1PTl5/arcgis/rest/services/4pm_parcel_dots_8_1_19/FeatureServer",
+        outFields: ["*"],
+        popupTemplate: template
+      });
+  
+  var parcels10_4pm = new FeatureLayer({
+        url: "https://services2.arcgis.com/bB9Y1bGKerz1PTl5/arcgis/rest/services/ten_person_parcels_4pm/FeatureServer"
+      });
+  
+  var symbol = {
+          type: "simple-marker",
+  style: "circle",
+  color: "cyan",
+  size: "10px",
+        };
+  
+  parcels10_4pm.renderer = {
+        type: "simple", // autocasts as new SimpleRenderer()
+        symbol: symbol
+      };
+  
+  
+  
+        map.add(parcels_4pm);
+        map.add(parcels10_4pm);
+  
+ 
       });
